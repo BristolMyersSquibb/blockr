c351f854f346c65dadc92c3cf1bfe373a4b306da
DivadNojnarg <dgranjon@ymail.com>
2023-10-10 18:09:41 +0200

    start add_block

--- R/stack.R
+++ R/stack.R
@@ -30,6 +30,45 @@ new_stack <- function(..., name = rand_names()) {
   structure(blocks, name = name, class = "stack")
 }
 
+#' Add block to a stack
+#'
+#' This is to be called oustide the stack by
+#' other modules.
+#'
+#' @param stack stack to update. See \link{new_stack}.
+#' @param block Block to insert.
+#' @param position Where to insert the new block. If NULL,
+#' the block will be added in the last position.
+#'
+#' @return Invisibly returns the stack.
+#' @export
+add_block <- function(stack, block, position = NULL) {
+  stopifnot(length(stack) > 0)
+
+  last <- stack[[length(stack)]]
+  # For now, we won't be able to insert a block
+  # after a plot block. In a later version, we may imagine
+  # have multiple block plot per stack so we'll have to revisit
+  # this ...
+  if (inherits(last, "plot_block")) {
+    stop("Can't insert a block below a plot block.")
+  }
+  if (is.null(position)) {
+    # inject new block + pass in data from previous block
+    stack[[length(stack) + 1]] <- do.call(
+      block,
+      list(stack[[length(stack)]])
+    )
+  } else {
+    tmp <- do.call(
+      block,
+      list(stack[[position]])
+    )
+    stack <- append(stack, tmp, position)
+  }
+  invisible(stack)
+}
+
 #' @param stack An object inheriting form `"stack"`
 #' @rdname new_stack
 #' @export
