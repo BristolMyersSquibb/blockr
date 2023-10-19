833ddedcaa4e80a83e1e1f723c8fa6b72e3a299f
John Coene <jcoenep@gmail.com>
2023-10-19 14:24:59 +0200

    fix: remove icon

--- srcjs/shiny.js
+++ srcjs/shiny.js
@@ -3,6 +3,8 @@ import { remove } from "./remove-stack.js";
 
 Shiny.addCustomMessageHandler("blockr-bind-stack", (msg) => {
   const stack = `#${msg.stack}`;
-  remove(stack);
-  collapse(stack);
+  setTimeout(() => {
+    remove(stack);
+    collapse(stack);
+  }, 500);
 });
