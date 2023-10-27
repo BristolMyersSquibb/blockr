0e37851b40b6d613de2c0260a6a91ec41dc015f9
DivadNojnarg <dgranjon@ymail.com>
2023-10-27 14:52:36 +0200

    re-include shinyvalidate

--- R/server.R
+++ R/server.R
@@ -81,6 +81,7 @@ generate_server.data_block <- function(x, id, ...) {
 #' @param in_dat Reactive input data
 #' @rdname generate_server
 #' @export
+#' @import shinyvalidate
 generate_server.transform_block <- function(x, in_dat, id, ...) {
 
   obs_expr <- function(x) {
