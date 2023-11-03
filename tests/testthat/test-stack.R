4c955f71a09a942798c02f4f428b361bf9fe51e9
DivadNojnarg <dgranjon@ymail.com>
2023-11-03 09:05:11 +0100

    basic shinytest2 driver for serve_stack

--- tests/testthat/test-stack.R
+++ tests/testthat/test-stack.R
@@ -7,3 +7,19 @@ test_that("stacks", {
   expect_s3_class(stack, "stack")
   expect_type(stack, "list")
 })
+
+test_that("serve stacks", {
+  stack <- new_stack(
+    new_data_block,
+    new_filter_block
+  )
+
+  app <- serve_stack(stack)
+  expect_s3_class(app, "shiny.appobj")
+
+  # App starts (blockr needs to be installed)
+  driver <- shinytest2::AppDriver$new(app, name = "app-starts")
+  socket_state <- driver$get_js("Shiny.shinyapp.$socket.readyState")
+  expect_equal(socket_state, 1)
+  driver$stop()
+})
