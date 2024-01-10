0b4bd6b1f3fde426eb9d436ce5fc5b043482995a
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2024-01-10 11:41:03 +0100

    add constructive-based ser/deser

--- R/json.R
+++ R/json.R
@@ -3,25 +3,44 @@
 #' Object serialization as JSON.
 #'
 #' @param x Object to (de)serialize
-#' @param ... Forwarded to [jsonlite::toJSON()]
 #'
 #' @export
-to_json <- function(x, ...) {
+to_json <- function(x) {
   UseMethod("to_json")
 }
 
-#' @rdname to_json
-#' @export
-to_json.field <- function(x, ...) {
+to_json_impl <- function(x) {
   jsonlite::toJSON(
-    list(content = unclass(x), attributes = attributes(x)),
-    ...
+    list(
+      payload = paste0(constructive::construct(x)[["code"]], collapse = "\n"),
+      blockr = as.character(utils::packageVersion("blockr"))
+    )
   )
 }
 
 #' @rdname to_json
 #' @export
-from_json <- function(x, ...) {
-  res <- jsonlite::fromJSON(x, ...)
-  `attributes<-`(res[["content"]], res[["attributes"]])
+to_json.field <- to_json_impl
+
+#' @rdname to_json
+#' @export
+to_json.block <- to_json_impl
+
+#' @rdname to_json
+#' @export
+to_json.stack <- to_json_impl
+
+#' @rdname to_json
+#' @export
+to_json.workspace <- to_json_impl
+
+#' @rdname to_json
+#' @export
+from_json <- function(x) {
+
+  res <- jsonlite::fromJSON(x)
+
+  stopifnot(setequal(names(res), c("payload", "blockr")))
+
+  eval(parse(text = res[["payload"]]))
 }
--- tests/testthat/test-json.R
+++ tests/testthat/test-json.R
@@ -1,6 +1,27 @@
-test_that("json", {
+test_that("json ser/deser for fields", {
 
   x <- new_string_field("foo")
+
+  expect_identical(from_json(to_json(x)), x)
+
+  y <- new_select_field("a", letters)
+
+  expect_identical(from_json(to_json(y)), y)
+})
+
+test_that("json ser/deser for blocks", {
+
+  x <- new_data_block("")
+
+  expect_identical(from_json(to_json(x)), x)
+})
+
+test_that("json ser/deser for stacks", {
+
+  x <- new_stack(
+    new_data_block,
+    new_filter_block
+  )
 
   expect_identical(from_json(to_json(x)), x)
 })
