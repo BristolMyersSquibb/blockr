4c21b16eaaf2f185bad474da80a23cc79dcea3a7
Christoph Sax <christoph.sax@gmail.com>
2024-01-19 13:24:10 +0700

    layout tweak

--- R/admiral.R
+++ R/admiral.R
@@ -122,3 +122,41 @@ admiral_derive_param_computed_block <- function(data, ...) {
   initialize_block(new_admiral_derive_param_computed_block(data, ...), data)
 }
 
+
+
+# A simple, one column layout
+#
+# we need the fields, not the rendered fields, that's why I can't use the layout
+# methods
+ui_fields.admiral_derive_param_computed_block <- function(x, ns, inputs_hidden, ...) {
+
+  html_card <- function(field, field_name) {
+    div(
+      div(
+        tags$h5(attr(field, "title")),
+        div(
+          class = c("text-muted small mb-2"),
+          attr(field, "descr")
+        )
+      ),
+      tags$div(
+        class = "w-100",
+        ui_input(field, id = ns(field_name), name = NULL)
+      )
+    )
+  }
+
+  cards <- Map(
+    html_card,
+    field = x,
+    field_name = names(x)
+  )
+
+  div(
+    class = sprintf("block-inputs %s", inputs_hidden),
+    div(cards, class = "mt-3")
+  )
+
+}
+
+
