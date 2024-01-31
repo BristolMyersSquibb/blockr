66f704dbc1b139592c563f20aa50a4ac082c839d
DivadNojnarg <dgranjon@ymail.com>
2024-01-31 22:59:10 +0100

    typo

--- inst/examples/workspace/app.R
+++ inst/examples/workspace/app.R
@@ -5,4 +5,4 @@ names(stacks) <- paste("stack", 1:3, sep = "-")
 
 do.call(set_workspace, args = c(stacks, list(title = "My workspace")))
 
-serve_worskpace(clear = FALSE)
+serve_workspace(clear = FALSE)
