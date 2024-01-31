04afd08126570c5015d40129bb1f58da01ab2f59
John Coene <jcoenep@gmail.com>
2024-01-31 15:58:37 +0100

    feat: layout fields on closes #237 and collapses when too many fields closes #236

--- R/layout.R
+++ R/layout.R
@@ -1,5 +1,85 @@
+is_whole <- \(n) {
+  ((n * 10L) %% 10L) == 0L
+}
+
+#' Layout fields on rows.
+#' @keywords internal
+layout_fields <- function(fields) {
+  # split into groups of 6 => max inputs per row
+  split(fields, ceiling(seq_along(fields) / 6)) |>
+    lapply(\(fields) {
+      # we divide by 3
+      width <- 12 / length(fields)
+
+      # it's simple if the number is whole
+      if (is_whole(width)) {
+        class <- sprintf("col-%d", width)
+
+        layout <- div(
+          class = "row",
+          fields |>
+            lapply(\(field) div(class = class, field)) |>
+            unname()
+        )
+
+        return(layout)
+      }
+
+      width <- ceiling(width)
+      indices <- seq_along(fields)
+
+      div(
+        class = "row",
+        indices |>
+          lapply(\(index) {
+            w <- width
+
+            if (index == length(fields)) {
+              w <- width + 1
+            }
+
+            div(
+              class = sprintf("col-%s", w),
+              fields[[index]]
+            )
+          }) |>
+          unname()
+      )
+    })
+}
+
+#' Default layout for fields
+#' Default layout for fields, places fields on rows
+#' and collapses parts of inputs when there are too many.
+#' @param fields List of fields.
+#' @param ... Currently ignored.
+#' @keywords internal
 default_layout_fields <- function(fields, ...) {
-  unname(fields)
+  # we remove hidden fields
+  fields <- fields[lengths(fields) > 0L]
+
+  if (length(fields) < 6L) {
+    return(layout_fields(fields))
+  }
+
+  id <- rand_names()
+
+  fields_shown <- fields[1:6L]
+  fields_hidden <- fields[7L:length(fields)]
+
+  list(
+    fields_shown |> layout_fields(),
+    tags$a(
+      `data-bs-toggle` = "collapse",
+      href = sprintf("#%s", id),
+      "show more inputs"
+    ),
+    div(
+      id = id,
+      class = "collapse",
+      fields_hidden |> layout_fields()
+    )
+  )
 }
 
 plot_layout_fields <- function(fields, ...) {
