66d408d43c03b8bb033ac00c9686544b17ca4f30
John Coene <jcoenep@gmail.com>
2024-03-22 17:39:47 +0100

    fix: retrieve batches

--- srcjs/registry/scroll.js
+++ srcjs/registry/scroll.js
@@ -1,5 +1,7 @@
 import { renderPills } from "./render";
 
+const BATCH = 20;
+
 export const unbindScroll = (params) => {
   $(`#${params.ns}-scrollable`).off("scroll");
 };
@@ -9,19 +11,38 @@ export const bindScroll = (params, endpoints) => {
 
   unbindScroll(params);
 
-  $(`#${params.ns}-scrollable`).on("scroll", (e) => {
+  $(`#${params.ns}-scrollable`).on("scroll", () => {
     unbindScroll(params);
+    const n = getNBlocks(params.ns);
     endpoints.fetchLeast().then((data) => {
-      renderPills(params, data);
+      const to = n + BATCH;
+      if (to > data.length) return;
+
+      renderPills(params, data.slice(n, to));
       bindScroll(params);
     });
   });
 };
 
 async function fetchUntilScrollable(params, endpoints) {
+  const n = getNBlocks(params.ns);
+
   return endpoints.fetchLeast().then((data) => {
     if (!data.length) return;
 
-    renderPills(params, data);
+    const to = n + BATCH;
+    if (to > data.length) return;
+
+    renderPills(params, data.slice(n, to));
+    if (
+      $(`#${params.ns}-scrollable-child`).height() <=
+      $(`#${params.ns}-scrollable`).height()
+    ) {
+      fetchUntilScrollable(params, endpoints);
+    }
   });
 }
+
+const getNBlocks = (ns) => {
+  return $(`#${ns}-scrollable`).find(".add-block").length;
+};
