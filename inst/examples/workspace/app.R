a44f6f34804990c277588d4d4834bbe3fb45708b
DivadNojnarg <dgranjon@ymail.com>
2024-04-29 17:43:49 +0900

    fix workspace without stack issue

--- inst/examples/workspace/app.R
+++ inst/examples/workspace/app.R
@@ -1,9 +1,6 @@
 # library(blockr)
 devtools::load_all()
 
-stacks <- lapply(1:3, \(x) new_stack(data_block))
-names(stacks) <- paste("stack", 1:3, sep = "-")
-
-do.call(set_workspace, args = c(stacks, list(title = "My workspace")))
+do.call(set_workspace, args = list(title = "My workspace"))
 
 serve_workspace(clear = FALSE)
