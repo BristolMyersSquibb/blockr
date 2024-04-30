db8e227a39be81929e39bb393e0e1586c2ec24f1
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2024-04-30 14:39:54 +0200

    rm 2nd ctor for blocks

--- R/mutate-block.R
+++ R/mutate-block.R
@@ -26,9 +26,8 @@ mutate_expr <- function(value = c(a = "2.1", b = "4.5")) {
 #' Leverages the \link{keyvalue_field}
 #'
 #' @param value Default value.
-#' @inheritParams select_block
+#' @inheritParams new_select_block
 #' @export
-#' @rdname mutate_block
 new_mutate_block <- function(data, value = NULL, ...) {
   fields <- list(
     value = new_keyvalue_field(value = value),
@@ -41,10 +40,4 @@ new_mutate_block <- function(data, value = NULL, ...) {
     ...,
     class = c("mutate_block", "transform_block")
   )
-}
-
-#' @rdname mutate_block
-#' @export
-mutate_block <- function(data, ...) {
-  initialize_block(new_mutate_block(data, ...), data)
 }
