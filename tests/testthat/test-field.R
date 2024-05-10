84a8aeb1bd78bc1866843495727b0b26ffb46dc7
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2024-05-10 15:23:09 +0200

    rm dual field ctors

--- tests/testthat/test-field.R
+++ tests/testthat/test-field.R
@@ -1,66 +1,84 @@
 test_that("string fields", {
 
-  field <- string_field("foo")
+  field <- new_string_field("foo")
 
   expect_s3_class(field, "string_field")
   expect_type(field, "list")
   expect_identical(value(field), "foo")
 
-  field <- string_field(1)
+  field <- new_string_field(1)
 
   expect_s3_class(field, "string_field")
   expect_type(field, "list")
+
+  field <- validate_field(field)
+
   expect_identical(value(field), "")
 })
 
 test_that("select fields", {
 
-  field <- select_field("a", letters)
+  field <- new_select_field("a", letters)
 
   expect_s3_class(field, "select_field")
   expect_type(field, "list")
   expect_identical(value(field), "a")
 
-  field <- select_field("aa", letters)
+  field <- new_select_field("aa", letters)
 
   expect_s3_class(field, "select_field")
   expect_type(field, "list")
+
+  field <- validate_field(field)
+
   expect_identical(value(field), character())
 
-  field <- select_field(1, letters)
+  field <- new_select_field(1, letters)
 
   expect_s3_class(field, "select_field")
   expect_type(field, "list")
+
+  field <- validate_field(field)
+
   expect_identical(value(field), character())
 })
 
 test_that("range fields", {
 
-  field <- range_field(min = 0, max = 10)
+  field <- new_range_field(min = 0, max = 10)
 
   expect_s3_class(field, "range_field")
   expect_type(field, "list")
+
+  field <- validate_field(field)
+
   expect_identical(value(field), c(0, 10))
 })
 
 test_that("numeric fields", {
 
-  field <- numeric_field(min = 0, max = 10)
+  field <- new_numeric_field(min = 0, max = 10)
 
   expect_s3_class(field, "numeric_field")
   expect_type(field, "list")
   expect_identical(value(field), numeric())
 
-  field <- numeric_field(value = 200, min = 0, max = 10)
+  field <- validate_field(
+    new_numeric_field(value = 200, min = 0, max = 10)
+  )
+
   expect_identical(value(field), 10)
 
-  field <- numeric_field(value = -10, min = 0, max = 10)
+  field <- validate_field(
+    new_numeric_field(value = -10, min = 0, max = 10)
+  )
+
   expect_identical(value(field), 0)
 })
 
 test_that("submit fields", {
 
-  field <- submit_field()
+  field <- new_submit_field()
 
   expect_s3_class(field, "submit_field")
   expect_type(field, "list")
@@ -69,7 +87,7 @@ test_that("submit fields", {
 
 test_that("switch field", {
 
-  field <- switch_field()
+  field <- new_switch_field()
 
   expect_s3_class(field, "switch_field")
   expect_type(field, "list")
@@ -78,7 +96,7 @@ test_that("switch field", {
 
 test_that("upload field", {
 
-  field <- upload_field()
+  field <- new_upload_field()
 
   expect_s3_class(field, "upload_field")
   expect_type(field, "list")
@@ -87,7 +105,7 @@ test_that("upload field", {
 
 test_that("filesbrowser field", {
 
-  field <- filesbrowser_field()
+  field <- new_filesbrowser_field()
 
   expect_s3_class(field, "filesbrowser_field")
   expect_type(field, "list")
