da81ac3ce5389d6d6ce8eae5f3d24befa4e23f88
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-11-14 17:50:29 +0100

    namespace fix

--- R/run.R
+++ R/run.R
@@ -7,15 +7,22 @@
 #' @rdname run_app
 #' @export
 run_app <- function(..., extensions = blockr.dag::new_dag_extension()) {
+
+  get_plugs <- function(x) {
+
+    default <- blockr.core::board_plugins(x)
+
+    hit  <- match("preserve_board", names(default), nomatch = NA_integer_)
+
+    if (is.na(hit)) {
+      return(default)
+    }
+
+    c(default[-hit], blockr.session::manage_session())
+  }
+
   blockr.core::serve(
     blockr.dock::new_dock_board(..., extensions = extensions),
-    plugins = function(x) {
-      default <- board_plugins(x)
-      hit  <- match("preserve_board", names(default), nomatch = NA_integer_)
-      if (is.na(hit)) {
-        return(default)
-      }
-      c(default[-hit], blockr.session::manage_session())
-    }
+    plugins = get_plugs
   )
 }
